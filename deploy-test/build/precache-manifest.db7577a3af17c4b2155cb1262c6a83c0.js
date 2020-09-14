1286b6125f902b9073fa469beab5cfe0f9c1e63a
easyfordev <easyfordev8@gmail.com>
2020-09-14 20:39:06 +0900

    Add build files

---
+++ deploy-test/build/precache-manifest.db7577a3af17c4b2155cb1262c6a83c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdf52794c9d3d3eb869f6677398a401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0abf3dd24f4d5b56c0df",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a98b18484ac687725bf",
+    "url": "/static/js/2.d8428b5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8428b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0abf3dd24f4d5b56c0df",
+    "url": "/static/js/main.73c11027.chunk.js"
+  },
+  {
+    "revision": "bad2a1cd62f11641430e",
+    "url": "/static/js/runtime-main.fc297e79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
